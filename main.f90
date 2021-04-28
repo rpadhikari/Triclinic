e3eb9f4a02610b64187b991054bc09356e6cd609
Rajendra Adhikari <rpadhikari@gmail.com>
2021-04-28 14:25:28 +0545

    updates on fractional coordinates

--- main.f90
+++ main.f90
@@ -2,8 +2,9 @@ program main
   implicit none
   integer(2) i
   real(8) a, b, c, alpha, beta, gamma
+  real(8), parameter :: a2b=0.5291770d0
   real(8) v1(3), v2(3), v3(3)
-  real(8), parameter :: pi=3.1415926535897932
+  real(8), parameter :: pi=3.14159265358979320d0
   open(1, file='in.dat',action='read', status='old')
   read(1,*) a, b, c, alpha, beta, gamma
   close(1)
@@ -14,6 +15,10 @@ program main
   write(*,10) (v1(i), i=1, 3)
   write(*,10) (v2(i), i=1, 3)
   write(*,10) (v3(i), i=1, 3)
+  write(*,*) ' vectors in fractional coordinates'
+  write(*,10) (v1(i)/a, i=1, 3)
+  write(*,10) (v2(i)/a, i=1, 3)
+  write(*,10) (v3(i)/a, i=1, 3)
+  write(*,*) 'value of cellmd(1) = ', a/a2b 
   10 format(3f18.10)
 end program main
-
